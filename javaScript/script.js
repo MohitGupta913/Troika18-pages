71b7981e44f31e32a7ec8b6853cf416e55e94066
Mohit.Gupta <mohitguptamt123@gmail.com>
2017-12-12 12:59:09 +0530

    navbar color changed

--- javaScript/script.js
+++ javaScript/script.js
@@ -24,6 +24,24 @@
           $('.navbar-toggle:visible').click();
       });
 
+
+    function checkScroll(){
+        var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+    
+        if($(window).scrollTop() > startY){
+            $('.navbar').addClass("scrolled");
+        }else{
+            $('.navbar').removeClass("scrolled");
+        }
+    }
+    
+    if($('.navbar').length > 0){
+        $(window).on("scroll load resize", function(){
+            checkScroll();
+        });
+    }
+
+
 //Login/Register button javascript
     $(document).ready(function() {
         $('.beta-home-button').bind("mouseover", function(){
